1903c96cfb76dde13ddb6a158e27eb610a6bc752
Mostafa <mustafa.husseiny@gmail.com>
2020-07-06 14:47:38 +0200

    class

--- confiy.js
+++ confiy.js
@@ -1,1 +1,27 @@
+"use strict";
 
+const structure = `
+<div id = 'cons'>
+  Confiy class
+</div>
+<style>
+#cons {
+    width: 300px;
+    height: 50px;
+    background-color: lightgray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
+`;
+class Idra extends HTMLElement {
+    constructor() {
+        super();
+        this.attachShadow( {mode: 'open'} );
+        const root = this.shadowRoot;
+        root.innerHTML = structure;
+    }
+}
+
+window.customElements.define(Idra, 'confiy-log');
